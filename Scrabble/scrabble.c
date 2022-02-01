5d6f9866a7ecd65908f4a41a76ebd1e4d92321c6
Yanius <zhzh-1987@mail.ru>
2022-02-01 19:03:59 +0700

    scrabble1_alternate commit

--- Scrabble/scrabble.c
+++ Scrabble/scrabble.c
@@ -43,6 +43,7 @@ int compute_score(string word)
     //arrays for upper and lower letters
     int up_letters[] = { 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90 };
     int low_letters[] = { 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122 };
+    int temp_points[] = {};
     //counter
     for (int i = 0; i < strlen(word); i++)
     {
@@ -53,7 +54,8 @@ int compute_score(string word)
             {
                 if (word[i] == up_letters[n])
                 {
-                    score += POINTS[n];
+                    temp_points[i] = POINTS[n];
+                    score += POINTS[i];
                 }
             }
 
@@ -65,7 +67,8 @@ int compute_score(string word)
             {
                 if (word[i] == low_letters[n])
                 {
-                    score += POINTS[n];
+                    temp_points[i] = POINTS[n];
+                    score += POINTS[i];
                 }
             }
         }
